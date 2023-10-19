7309e1252e94e169ab17603d3a3cb4e5defd8ceb
Sander Bell <thesanderbell@gmail.com>
2023-10-19 22:56:38 +0700

    Fixed Pinch

--- src/components/MobileWarning.vue
+++ src/components/MobileWarning.vue
@@ -1,17 +1,17 @@
 <template>
     <div id="mobile-warning">
-        <img draggable="false" :src="pinch" />
+        <img draggable="false" :src="image" />
         <h1>Please open this site on a larger screen</h1>
     </div>
 </template>
   
 <script>
-import pinch from "../assets/pinch.png";
+import image from "../assets/pinch.png";
 
 export default {
     data: function () {
         return {
-            image: pinch
+            image: image
         };
     }
 };
