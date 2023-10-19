ec39610a5133f8fde1c1cd98f67e5e124afda224
Sander Bell <thesanderbell@gmail.com>
2023-10-19 22:55:00 +0700

    Updated Mobile Warning

--- src/components/MobileWarning.vue
+++ src/components/MobileWarning.vue
@@ -1,17 +1,17 @@
 <template>
     <div id="mobile-warning">
-        <img draggable="false" :src="image" />
-        <h1>Please open this site on desktop</h1>
+        <img draggable="false" :src="pinch" />
+        <h1>Please open this site on a larger screen</h1>
     </div>
 </template>
   
 <script>
-import image from "../assets/pinch.png";
+import pinch from "../assets/pinch.png";
 
 export default {
     data: function () {
         return {
-            image: image
+            image: pinch
         };
     }
 };
